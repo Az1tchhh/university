e09a5397dcd27ce90e63f7e32bd8ea1fde3a4865
Az1tchhh <azamatbazarbai007@gmail.com>
2023-11-19 15:19:25 +0600

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
+import {MainPageComponent} from "./main-page/main-page.component";
+import {TestComponent} from "./test/test.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:WelcomePageComponent},
+  {path:'main', component:MainPageComponent},
+  {path:'test', component:TestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { RouterModule } from "@angular/router";
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    MainPageComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
